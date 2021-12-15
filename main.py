2e24f3cafe3e275c0c951b47e83c6d9b27be4e9b
edentik <edentik@yahoo.de>
2021-12-15 21:42:34 +0100

    some else statements

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
+
+
+def check_resources(input_resources, product_ingredients):
+    for resource in input_resources:
+        if resource > product_ingredients:
+            return f""
+
+# def calculalte_coins(input_quarters, input_dimes, input_nickles, input_pennies):
+#     input_quarters * 0.25
+#     input_dimes * 0.10
+#     input_nickles * 0.05
+#     input_pennies * 0.01
+
 MENU = {
     "espresso": {
         "ingredients": {
@@ -31,16 +44,29 @@ resources = {
     "money": 0,
 }
 
-# TODO: Maschieneninhalt -> 300ml Wasser, 200ml Milch, 100g Kaffee
-#   Coins: Penny = 1 cent, Nickel = 5 cents, Dime = 10 cents, Quarter = 25 cents
+# TODO: Coins: Penny = 1 cent, Nickel = 5 cents, Dime = 10 cents, Quarter = 25 cents
 
 # TODO: Print report -> How much resources left?
-wish = input("What would you like? espresso/latte/cappuccino? ")
-if wish == "report":
+drink = input("What would you like? espresso/latte/cappuccino? ")
+if drink == "report":
     print(f"Water: {resources['water']}ml\n"
           f"Milk: {resources['milk']}ml\n"
           f"Coffee: {resources['coffee']}g\n"
           f"Money: ${resources['money']}\n")
+else:
+    # FIXME:
+    print("Please insert coins")
+    quarters = int(input(f"How many quarters?: ")) * 0.25
+    dimes = int(input(f"How many dimes?: ")) * 0.10
+    nickles = int(input(f"How many nickles?: ")) * 0.05
+    pennies = int(input(f"How many pennies?: ")) * 0.01
+    budget = quarters + dimes + nickles + pennies
+    drink = MENU[drink]
+    price = drink[cost]
+    print(price)
+    # print(budget - price)
+
+
 
 
 # TODO: Check if resources sufficient
